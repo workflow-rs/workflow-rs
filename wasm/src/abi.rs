4acb780833ac853dc3c57ffa0b8fafdd5a09c57a
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-03-20 02:04:34 -0400

    convert ABI utils to use Error enums

--- wasm/src/abi.rs
+++ wasm/src/abi.rs
@@ -1,5 +1,6 @@
 //! Functions to obtain Rust object references from WASM ABI.
 
+use crate::error::Error;
 use wasm_bindgen::convert::RefFromWasmAbi;
 use wasm_bindgen::prelude::*;
 pub use workflow_wasm_macros::{ref_from_abi, ref_from_abi_as_option, TryFromJsValue};
@@ -9,25 +10,19 @@ pub use workflow_wasm_macros::{ref_from_abi, ref_from_abi_as_option, TryFromJsVa
 /// Only basic sanity checks are performed. If user passes and invalid
 /// object containing `ptr` property (i.e. another Rust-wasm-bindgen-created object)
 /// the application will yield a memory access violation.
-pub unsafe fn ref_from_abi<T>(js: &JsValue) -> std::result::Result<T, JsValue>
+pub unsafe fn ref_from_abi<T>(js: &JsValue) -> std::result::Result<T, Error>
 where
     T: RefFromWasmAbi<Abi = u32> + Clone,
 {
     if !js.is_object() {
-        return Err(JsValue::from(
-            "ref_from_abi_safe(): supplied argument is not a class object",
-        ));
+        return Err(Error::NotAnObject);
     }
 
     let ptr = ::js_sys::Reflect::get(js, &JsValue::from_str("ptr"))?;
     if ptr.is_falsy() {
-        return Err(JsValue::from(
-            "ref_from_abi(): WASM API received an invalid JavaScript object (the object is not a WASM ABI pointer)",
-        ));
+        return Err(Error::NotWasmAbiPointer);
     }
-    let ptr_u32: u32 = ptr.as_f64().ok_or(JsValue::from(
-        "reg_from_abi(): WASM API received an invalid JavaScript object (the object is not a WASM ABI pointer)",
-    ))? as u32;
+    let ptr_u32: u32 = ptr.as_f64().ok_or(Error::NotWasmAbiPointer)? as u32;
     let instance_ref = unsafe { T::ref_from_abi(ptr_u32) };
     Ok(instance_ref.clone())
 }
@@ -36,44 +31,38 @@ where
 /// This function validates the supplied object by comparing its `constructor.name` value to the supplied
 /// `class` name. You can use this function in two forms: `ref_from_abi_safe("SomeStruct", jsvalue)` or
 /// via a macro `ref_from_abi!(SomeStruct,jsvalue)`.
-pub fn ref_from_abi_safe<T>(class: &str, js: &JsValue) -> std::result::Result<T, JsValue>
+pub fn ref_from_abi_safe<T>(class: &str, js: &JsValue) -> std::result::Result<T, Error>
 where
     T: RefFromWasmAbi<Abi = u32> + Clone,
 {
     if !js.is_object() {
-        return Err(JsValue::from(format!(
-            "ref_from_abi_safe(): supplied argument is not of expected class type `{class}`"
-        )));
+        return Err(Error::NotAnObjectOfClass(class.to_string()));
     }
 
     let ctor = ::js_sys::Reflect::get(js, &JsValue::from_str("constructor"))?;
     if ctor.is_falsy() {
-        return Err(JsValue::from(format!("ref_from_abi_safe(): unable to obtain object constructor (for expected class `{class}`)")));
+        return Err(Error::NoConstructorOfClass(class.to_string()));
     } else {
         let name = ::js_sys::Reflect::get(&ctor, &JsValue::from_str("name"))?;
         if name.is_falsy() {
-            return Err(JsValue::from(
-                format!("ref_from_abi_safe(): unable to obtain object constructor name (for expected class `{class}`)")
-            ));
+            return Err(Error::UnableToObtainConstructorName(class.to_string()));
         } else {
-            let name = name.as_string().ok_or(JsValue::from(format!("ref_from_abi_safe(): unable to obtain object constructor name (for expected class `{class}`)")))?;
+            let name = name
+                .as_string()
+                .ok_or(Error::UnableToObtainConstructorName(class.to_string()))?;
             if name != class {
-                return Err(JsValue::from(format!(
-                    "ref_from_abi_safe(): object constructor `{name}` does not match expected class `{class}`"
-                )));
+                return Err(Error::ClassConstructorMatch(name, class.to_string()));
             }
         }
     }
 
     let ptr = ::js_sys::Reflect::get(js, &::wasm_bindgen::JsValue::from_str("ptr"))?;
     if ptr.is_falsy() {
-        return Err(JsValue::from(
-            "ref_from_abi(): WASM API received an invalid JavaScript object (the object is not a WASM ABI pointer)",
-        ));
+        return Err(Error::NotWasmAbiPointerForClass(class.to_string()));
     }
-    let ptr_u32: u32 = ptr.as_f64().ok_or(::wasm_bindgen::JsValue::from(
-        "reg_from_abi(): WASM API received an invalid JavaScript object (the object is not a WASM ABI pointer)",
-    ))? as u32;
+    let ptr_u32: u32 = ptr
+        .as_f64()
+        .ok_or(Error::NotWasmAbiPointerForClass(class.to_string()))? as u32;
     let instance_ref = unsafe { T::ref_from_abi(ptr_u32) };
     Ok(instance_ref.clone())
 }
