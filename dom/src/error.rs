77709581228f29049358bff17fb6e584cd8650b0
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-06-22 05:27:25 +0300

    Wrap JsValue as Sendable in dom errors

--- dom/src/error.rs
+++ dom/src/error.rs
@@ -4,6 +4,7 @@
 use thiserror::Error;
 use wasm_bindgen::JsValue;
 use workflow_core::channel::RecvError;
+use workflow_wasm::sendable::Sendable;
 
 /// Errors return by the [`workflow_dom`](super) module
 #[derive(Error, Debug, Clone)]
@@ -13,7 +14,7 @@ pub enum Error {
     String(String),
     /// Error containing [`wasm_bindgen::JsValue`] value
     #[error("{0:?}")]
-    JsValue(JsValue),
+    JsValue(Sendable<JsValue>),
     #[error("{0}")]
     RecvError(RecvError), //#[from] workflow_core::channel::RecvError),
 }
@@ -35,7 +36,7 @@ impl From<&str> for Error {
 
 impl From<JsValue> for Error {
     fn from(v: JsValue) -> Self {
-        Self::JsValue(v)
+        Self::JsValue(Sendable(v))
     }
 }
 
