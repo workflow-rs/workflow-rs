17490ef899426aca2923dafdf8456c71bd803d5c
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-07-16 22:43:04 +0300

    nw globals

--- nw/src/global.rs
+++ nw/src/global.rs
@@ -7,8 +7,16 @@ extern "C" {
 
     #[wasm_bindgen(getter, static_method_of = Global, js_class = global, js_name = global)]
     fn get_global() -> Object;
+
+    #[wasm_bindgen(getter, catch, static_method_of = Global, js_class = globalThis, js_name = globalThis)]
+    fn get_global_this() -> Result<Object, JsValue>;
+
 }
 
 pub fn global() -> Object {
+    Global::get_global()
+}
+
+pub fn global_this() -> Object {
     Global::get_global()
 }
