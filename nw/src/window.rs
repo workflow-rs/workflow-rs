1b4308b312a802fb200e96783b494fb4f313db19
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-07-24 18:49:41 +0300

    wip - debugging window enumeration issue in --dev

--- nw/src/window.rs
+++ nw/src/window.rs
@@ -24,3 +24,30 @@ pub async fn get_all_async() -> Result<Vec<Window>> {
 
     Ok(result)
 }
+
+// this can be used to test...
+// pub async fn get_all_async() -> Result<Vec<Window>> {
+//     let (sender, receiver) = oneshot();
+//     let callback = Callback::new_with_args_2(move |windows: JsValue, v2: JsValue| {
+//         log_info!("windows: {:?}", windows);
+//         log_info!("v2: {:?}", v2);
+
+//         sender.try_send(Sendable(windows)).unwrap();
+//     });
+
+//     nw_sys::window::get_all(callback.as_ref());
+//     let windows = receiver.recv().await?;
+//     let length = Reflect::get(&windows, &"length".into())?;
+//     log_info!("length: {:?}", length);
+//     let length = length.as_f64().unwrap() as usize;
+//     let result = (0..length)
+//         .into_iter()
+//         .map(|index| {
+//             Reflect::get(&windows, &index.into())
+//                 .expect("failed to get nw window")
+//                 .unchecked_into::<Window>()
+//         })
+//         .collect::<Vec<_>>();
+
+//     Ok(result)
+// }
