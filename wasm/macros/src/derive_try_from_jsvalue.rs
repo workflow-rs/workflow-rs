cbfa877dfa211cb6999811fed2d47fb11d75bb20
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-08-10 22:47:22 +0300

    wasm - fix ptr to __wbg_ptr on TryFromJsValue

--- wasm/macros/src/derive_try_from_jsvalue.rs
+++ wasm/macros/src/derive_try_from_jsvalue.rs
@@ -260,7 +260,7 @@ pub fn derive_try_from_jsvalue(input: TokenStream) -> TokenStream {
                     .ok_or_else(|| "Failed to get classname".to_owned())?;
 
                 if object_classname.as_str() == classname {
-                    let ptr = ::js_sys::Reflect::get(js, &::wasm_bindgen::JsValue::from_str("ptr"))
+                    let ptr = ::js_sys::Reflect::get(js, &::wasm_bindgen::JsValue::from_str("__wbg_ptr"))
                         .map_err(|err| format!("{:?}", err))?;
                     let ptr_u32: u32 = ptr.as_f64().ok_or(::wasm_bindgen::JsValue::NULL)
                         .map_err(|err| format!("{:?}", err))?
