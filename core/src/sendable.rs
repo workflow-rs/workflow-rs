f783474375d1bba2adf7958be7d92e084b1c49cd
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-09-17 04:46:03 +0300

    remove Clone requirement from wasm/Sendable

--- core/src/sendable.rs
+++ core/src/sendable.rs
@@ -6,23 +6,20 @@ use std::fmt::Display;
 use wasm_bindgen::JsValue;
 
 ///
-/// Senable wrapper for JS primitives.
+/// Sendable wrapper for JS primitives.
 ///
 /// Wrapping any JS primitive (JsValue, JsString, JsArray, JsObject, etc.) in
 /// `Sendable<T>` wraps the value with the Send marker, making it transportable
 /// across "thread boundaries". In reality, this allows JS primitives to be
 /// used safely within a single-threaded WASM async environment (browser).
 ///
-#[derive(Clone, Debug)]
-pub struct Sendable<T>(pub T)
-where
-    T: Clone;
-unsafe impl<T> Send for Sendable<T> where T: Clone {}
-unsafe impl<T> Sync for Sendable<T> where T: Clone {}
+#[derive(Debug)]
+pub struct Sendable<T>(pub T);
+
+unsafe impl<T> Send for Sendable<T> {}
+unsafe impl<T> Sync for Sendable<T> {}
 
 impl<T> Sendable<T>
-where
-    T: Clone,
 {
     pub fn new(value: T) -> Self {
         Self(value)
@@ -34,8 +31,6 @@ where
 }
 
 impl<T> std::ops::Deref for Sendable<T>
-where
-    T: Clone,
 {
     type Target = T;
     fn deref(&self) -> &T {
@@ -44,8 +39,6 @@ where
 }
 
 impl<T> AsRef<T> for Sendable<T>
-where
-    T: Clone,
 {
     fn as_ref(&self) -> &T {
         &self.0
@@ -53,8 +46,6 @@ where
 }
 
 impl<T> AsMut<T> for Sendable<T>
-where
-    T: Clone,
 {
     fn as_mut(&mut self) -> &mut T {
         &mut self.0
@@ -62,8 +53,6 @@ where
 }
 
 impl<T> From<T> for Sendable<T>
-where
-    T: Clone,
 {
     fn from(t: T) -> Self {
         Sendable(t)
@@ -72,7 +61,7 @@ where
 
 impl<T> Display for Sendable<T>
 where
-    T: Clone + Display,
+    T: Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
@@ -81,14 +70,14 @@ where
 
 impl<T> From<Sendable<T>> for JsValue
 where
-    T: Clone + Into<JsValue>,
+    T: Into<JsValue>,
 {
     fn from(s: Sendable<T>) -> Self {
         s.0.into()
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct SendableFuture<T>(pub T);
 unsafe impl<T> Send for SendableFuture<T> {}
 unsafe impl<T> Sync for SendableFuture<T> {}
