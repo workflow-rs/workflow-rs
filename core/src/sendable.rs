ffd77c9e21c43c63a458843c6c1d380bbbc43617
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2023-09-17 15:26:18 +0300

    code formatting

--- core/src/sendable.rs
+++ core/src/sendable.rs
@@ -19,8 +19,7 @@ pub struct Sendable<T>(pub T);
 unsafe impl<T> Send for Sendable<T> {}
 unsafe impl<T> Sync for Sendable<T> {}
 
-impl<T> Sendable<T>
-{
+impl<T> Sendable<T> {
     pub fn new(value: T) -> Self {
         Self(value)
     }
@@ -30,30 +29,26 @@ impl<T> Sendable<T>
     }
 }
 
-impl<T> std::ops::Deref for Sendable<T>
-{
+impl<T> std::ops::Deref for Sendable<T> {
     type Target = T;
     fn deref(&self) -> &T {
         &self.0
     }
 }
 
-impl<T> AsRef<T> for Sendable<T>
-{
+impl<T> AsRef<T> for Sendable<T> {
     fn as_ref(&self) -> &T {
         &self.0
     }
 }
 
-impl<T> AsMut<T> for Sendable<T>
-{
+impl<T> AsMut<T> for Sendable<T> {
     fn as_mut(&mut self) -> &mut T {
         &mut self.0
     }
 }
 
-impl<T> From<T> for Sendable<T>
-{
+impl<T> From<T> for Sendable<T> {
     fn from(t: T) -> Self {
         Sendable(t)
     }
