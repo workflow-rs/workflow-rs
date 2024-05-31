f795e8f7d47bc642eca235a5b529732cdf90f3a8
1bananagirl <168954040+1bananagirl@users.noreply.github.com>
2024-05-31 16:12:34 +0000

    Fixes for Android and IOs platforms and websockets in cross-origin isolated environments (#8)
    * Added platform detect for Android and IOs, patch variable shadowing
    
    * Runtime detect cross-origin isolated browser environment. Websocket send non-shared ArrayBuffer in cross-origin isolated browser environment.
    
    * Array copy from method instead loop assign, wasm websocket mod inclusion to be visible for rust-analyzer.
    
    * file head comment prune (1 line too much)
    
    * Linting and checks. Filter for overrides for Flutter env.

--- websocket/src/client/mod.rs
+++ websocket/src/client/mod.rs
@@ -3,10 +3,11 @@
 //!
 
 use cfg_if::cfg_if;
+mod wasm;
+pub use wasm::WebSocketInterface as _WSI;
 
 cfg_if! {
     if #[cfg(target_arch = "wasm32")] {
-        mod wasm;
         use wasm::WebSocketInterface;
     } else {
         mod native;
